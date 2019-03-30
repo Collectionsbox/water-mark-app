50879703919cf61de6856a30933c711a680b015c
Yukiniro <yukiniro@hotmail.com>
2019-03-30 21:06:53 +0800

    Updates

---
+++ precache-manifest.63027b89b15782e0bb0268bac4f3c132.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "343ee25017cd9e97b12a",
+    "url": "/water-mark-app/static/js/runtime~main.727ea5ce.js"
+  },
+  {
+    "revision": "7d902344e825298ffd40",
+    "url": "/water-mark-app/static/js/main.0c2552f5.chunk.js"
+  },
+  {
+    "revision": "86be38ab8cfe2da8bc18",
+    "url": "/water-mark-app/static/js/2.2b6090f3.chunk.js"
+  },
+  {
+    "revision": "7d902344e825298ffd40",
+    "url": "/water-mark-app/static/css/main.cfb0069e.chunk.css"
+  },
+  {
+    "revision": "a59b66104d808d62ae230fcc50761c38",
+    "url": "/water-mark-app/index.html"
+  }
+];
